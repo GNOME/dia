9214d57bf7258250ef74d8247dc14e9466317e88
Nuno Ferreira <nmrf@rnl.ist.utl.pt>
1998-09-14 01:37:56 +0000

    Changes to work with srcdir != builddir.
    1998-09-14  Nuno Ferreira  <nmrf@rnl.ist.utl.pt>
    
    	* */Makefile.am (INCLUDES): Changes to work with srcdir !=
    	builddir.
    
    	* autogen.sh: New file. Copied from gtk and added a call to
    	libtoolize. Anyone knows why this isn't done in gtk?
    
    	* Removed autogenerated files.

---
+++ autogen.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+PROJECT=dia
+TEST_TYPE=-d
+FILE=objects
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile $PROJECT."
+	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+case $CC in
+*lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+libtoolize --copy --force
+
+aclocal $ACLOCAL_FLAGS
+
+# optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+
+automake $am_opt
+autoconf
+
+./configure "$@"
+
+echo 
+echo "Now type 'make' to compile $PROJECT."
+
