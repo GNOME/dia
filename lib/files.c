26f85afdffe7cc6e42a63dddac0785a72c609d9f
Alexander Larsson <alexl@src.gnome.org>
1998-10-02 17:14:47 +0000

    *blub*

--- lib/files.c
+++ lib/files.c
@@ -15,10 +15,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <math.h>
 
 #include "files.h"
