8ee6e71682eef6c77d64bce45f44e0982a57d4c0
James Henstridge <james@daa.com.au>
1999-10-14 08:42:16 +0000

    add new shapes to flowchart sheet.
    1999-10-14  James Henstridge  <james@daa.com.au>
    
    	* objects/flowchart/flowchart.c: add new shapes to flowchart sheet.
    
    	* object/flowchart/pixmaps/...: corresponding pixmaps for new shapes.
    
    	* objects/flowchart/offpageconn.shape:
    	* objects/flowchart/manualop.shape:
    	* objects/flowchart/preparation.shape:
    	* objects/flowchart/manualinput.shape:
    	* objects/flowchart/predefdproc.shape:
    	* objects/flowchart/terminal.shape: new shapes in the flowchart sheet.

--- objects/flowchart/flowchart.c
+++ objects/flowchart/flowchart.c
@@ -21,24 +21,42 @@
 #include "custom.h"
 
 #include "pixmaps/display.xpm"
-#include "pixmaps/document.xpm"
 #include "pixmaps/transaction.xpm"
+#include "pixmaps/offpageconn.xpm"
+#include "pixmaps/document.xpm"
+#include "pixmaps/manualop.xpm"
+#include "pixmaps/preparation.xpm"
+#include "pixmaps/manualinput.xpm"
+#include "pixmaps/predefdproc.xpm"
+#include "pixmaps/terminal.xpm"
 
 extern ObjectType fc_box_type;
 extern ObjectType pgram_type;
 extern ObjectType diamond_type;
 extern ObjectType fc_ellipse_type;
 static ObjectType *display_type = NULL;
-static ObjectType *doc_type = NULL;
 static ObjectType *transaction_type = NULL;
+static ObjectType *offpageconn_type = NULL;
+static ObjectType *doc_type = NULL;
+static ObjectType *manualop_type = NULL;
+static ObjectType *preparation_type = NULL;
+static ObjectType *manualinput_type = NULL;
+static ObjectType *predefdproc_type = NULL;
+static ObjectType *terminal_type = NULL;
 
 extern SheetObject box_sheetobj;
 extern SheetObject pgram_sheetobj;
 extern SheetObject diamond_sheetobj;
 extern SheetObject ellipse_sheetobj;
 static SheetObject *display_sheetobj = NULL;
-static SheetObject *doc_sheetobj = NULL;
 static SheetObject *transaction_sheetobj = NULL;
+static SheetObject *offpageconn_sheetobj = NULL;
+static SheetObject *doc_sheetobj = NULL;
+static SheetObject *manualop_sheetobj = NULL;
+static SheetObject *preparation_sheetobj = NULL;
+static SheetObject *manualinput_sheetobj = NULL;
+static SheetObject *predefdproc_sheetobj = NULL;
+static SheetObject *terminal_sheetobj = NULL;
 
 int get_version(void) {
   return 0;
@@ -56,16 +74,53 @@ void register_objects(void) {
   display_sheetobj->pixmap = display_xpm;
   object_register_type(display_type);
 
-  custom_object_load(SHAPE_DIR "/document.shape", &doc_type, &doc_sheetobj);
-  doc_type->pixmap = document_xpm;
-  doc_sheetobj->pixmap = document_xpm;
-  object_register_type(doc_type);
-
   custom_object_load(SHAPE_DIR "/transaction.shape", &transaction_type,
 		     &transaction_sheetobj);
   transaction_type->pixmap = transaction_xpm;
   transaction_sheetobj->pixmap = transaction_xpm;
   object_register_type(transaction_type);
+
+  custom_object_load(SHAPE_DIR "/offpageconn.shape", &offpageconn_type,
+		     &offpageconn_sheetobj);
+  offpageconn_type->pixmap = offpageconn_xpm;
+  offpageconn_sheetobj->pixmap = offpageconn_xpm;
+  object_register_type(offpageconn_type);
+
+  custom_object_load(SHAPE_DIR "/document.shape", &doc_type, &doc_sheetobj);
+  doc_type->pixmap = document_xpm;
+  doc_sheetobj->pixmap = document_xpm;
+  object_register_type(doc_type);
+
+  custom_object_load(SHAPE_DIR "/manualop.shape", &manualop_type,
+		     &manualop_sheetobj);
+  manualop_type->pixmap = manualop_xpm;
+  manualop_sheetobj->pixmap = manualop_xpm;
+  object_register_type(manualop_type);
+
+  custom_object_load(SHAPE_DIR "/preparation.shape", &preparation_type,
+		     &preparation_sheetobj);
+  preparation_type->pixmap = preparation_xpm;
+  preparation_sheetobj->pixmap = preparation_xpm;
+  object_register_type(preparation_type);
+
+  custom_object_load(SHAPE_DIR "/manualinput.shape", &manualinput_type,
+		     &manualinput_sheetobj);
+  manualinput_type->pixmap = manualinput_xpm;
+  manualinput_sheetobj->pixmap = manualinput_xpm;
+  object_register_type(manualinput_type);
+
+  custom_object_load(SHAPE_DIR "/predefdproc.shape", &predefdproc_type,
+		     &predefdproc_sheetobj);
+  predefdproc_type->pixmap = predefdproc_xpm;
+  predefdproc_sheetobj->pixmap = predefdproc_xpm;
+  object_register_type(predefdproc_type);
+
+  custom_object_load(SHAPE_DIR "/terminal.shape", &terminal_type,
+		     &terminal_sheetobj);
+  terminal_type->pixmap = terminal_xpm;
+  terminal_sheetobj->pixmap = terminal_xpm;
+  object_register_type(terminal_type);
+
 }
 
 void register_sheets(void) {
@@ -78,8 +133,14 @@ void register_sheets(void) {
   sheet_append_sheet_obj(sheet, &diamond_sheetobj);
   sheet_append_sheet_obj(sheet, &ellipse_sheetobj);
   sheet_append_sheet_obj(sheet, display_sheetobj);
-  sheet_append_sheet_obj(sheet, doc_sheetobj);
   sheet_append_sheet_obj(sheet, transaction_sheetobj);
+  sheet_append_sheet_obj(sheet, offpageconn_sheetobj);
+  sheet_append_sheet_obj(sheet, doc_sheetobj);
+  sheet_append_sheet_obj(sheet, manualop_sheetobj);
+  sheet_append_sheet_obj(sheet, preparation_sheetobj);
+  sheet_append_sheet_obj(sheet, manualinput_sheetobj);
+  sheet_append_sheet_obj(sheet, predefdproc_sheetobj);
+  sheet_append_sheet_obj(sheet, terminal_sheetobj);
 
   register_sheet(sheet);
 }
