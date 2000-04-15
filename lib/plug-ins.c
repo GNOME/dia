cbd94f0c5cf1a31449db72848b4778fbfdea644d
Asbjørn Pettersen <asbjoer@src.gnome.org>
2000-04-15 18:11:58 +0000

    add sys/types.h for os2

--- lib/plug-ins.c
+++ lib/plug-ins.c
@@ -21,9 +21,10 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
 
