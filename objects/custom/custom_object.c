82137efc5293185670c5faa119875f7abfe5bdb0
Cyrille Chepelov <cyrille@src.gnome.org>
2002-06-23 20:18:27 +0000

    using dia_font_new_from_style()
    	* objects/custom/custom_object.c:
    	using dia_font_new_from_style()

--- objects/custom/custom_object.c
+++ objects/custom/custom_object.c
@@ -1235,8 +1235,7 @@ custom_create(Point *startpoint,
             /* set default values for text style */
             if (!el->text.s.font_height) el->text.s.font_height = FONT_HEIGHT_DEFAULT;
             if (!el->text.s.font)
-                el->text.s.font = dia_font_new(BASIC_SANS_FONT,
-                                               STYLE_NORMAL,1.0);
+                el->text.s.font = dia_font_new_from_style(DIA_FONT_SANS,1.0);
             if (el->text.s.alignment == -1) el->text.s.alignment = TEXT_ALIGNMENT_DEFAULT;
             el->text.object = new_text(el->text.string, el->text.s.font, el->text.s.font_height,
 	        &el->text.anchor, &color_black, el->text.s.alignment);
