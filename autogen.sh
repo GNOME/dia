a7e46aa28abca12371b5caf0b0f4607edce127c1
Lars Clausen <lclausen@src.gnome.org>
2002-07-17 21:29:37 +0000

    AM_GLIB_GNU_GETTEXT

--- autogen.sh
+++ autogen.sh
@@ -32,7 +32,7 @@ DIE=0
 (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
 	echo
 	echo "You must have intltool installed to compile $PROJECT."
-        echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.16.tar.gz"
+	echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/intltool/intltool-0.16.tar.gz"
 	echo "(or a newer version if it is available)"
 	DIE=1
 }
@@ -56,26 +56,15 @@ test $TEST_TYPE $FILE || {
 
 if test -z "$*"; then
 	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+	echo "to pass any to it, please specify them on the $0 command line."
 fi
 
 case $CC in
 *lcc | *lcc\ *) am_opt=--include-deps;;
 esac
 
-echo "Running gettextize...  Ignore non-fatal messages."
-## Hmm, we specify --force here, since otherwise things don't
-## get added reliably, but we don't want to overwrite intl
-## while making dist.
-#echo "no" | gettextize --copy --force
-# finally, no, we don't try to force. Otherwise gettextize spends its time 
-# telling its life in po/ChangeLog.
-## It has to run with --force to make po/Makefile.in.in, though
-if [ -f po/Makefile.in.in ]; then
-  gettextize --copy
-else
-  gettextize --copy --force
-fi
+echo "Running gettextize..."
+glib-gettextize --copy --force
 
 echo "Running intltoolize"
 intltoolize --copy --force --automake
@@ -84,15 +73,13 @@ echo "Running libtoolize"
 libtoolize --copy --force
 
 aclocal $ACLOCAL_FLAGS
-
-autoheader$ACFLAVOUR
-
-automake -a $am_opt
-autoconf$ACFLAVOUR
+autoheader
+automake --add-missing $am_opt
+autoconf
 
 cd $ORIGDIR
 
 $srcdir/configure --enable-maintainer-mode --enable-db2html "$@"
 
-echo 
+echo
 echo "Now type 'make' to compile $PROJECT."
