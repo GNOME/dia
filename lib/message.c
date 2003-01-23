e0f4b2228a554b7f7dc0fddeb9db89a040e576d8
Lars Clausen <lclausen@src.gnome.org>
2003-01-23 22:45:51 +0000

    Destroy.

--- lib/message.c
+++ lib/message.c
@@ -36,6 +36,7 @@ typedef struct {
   GtkWidget *repeat_label;
   GList *repeats;
   GtkWidget *repeat_view;
+  GtkWidget *show_repeats;
 } DiaMessageInfo;
 
 static void
@@ -50,14 +51,93 @@ gtk_message_toggle_repeats(GtkWidget *button, gpointer *userdata) {
 }
 
 static void
+message_dialog_destroyed(GtkWidget *widget, gpointer userdata)
+{
+  DiaMessageInfo *msginfo = (DiaMessageInfo *)userdata;
+
+  msginfo->dialog = NULL;
+  msginfo->repeat_label = NULL;
+  msginfo->repeat_view = NULL;
+  msginfo->show_repeats = NULL;
+  
+}
+
+/** Set up a dialog for these messages.
+ * Msginfo may contain repeats, and should be filled out
+ */
+static void
+message_create_dialog(const gchar *title, DiaMessageInfo *msginfo, gchar *buf)
+{
+  GtkWidget *dialog = NULL;
+  GtkTextBuffer *textbuffer;
+  GtkMessageType type = GTK_MESSAGE_INFO;
+  GList *repeats;
+
+  /* quite dirty in order to not change Dia's message api */
+  if (title) {
+    if (0 == strcmp (title, _("Error")))
+      type = GTK_MESSAGE_ERROR;
+    else if (0 == strcmp (title, _("Warning")))
+      type = GTK_MESSAGE_WARNING;
+  }
+  if (msginfo->repeats != NULL)
+    buf = (gchar *)msginfo->repeats->data;
+  dialog = gtk_message_dialog_new (NULL, /* no parent window */
+				   0,    /* GtkDialogFlags */
+				   type,
+				   GTK_BUTTONS_CLOSE,
+				   buf);
+  if (title) {
+    gchar *real_title;
+
+    real_title = g_strdup_printf ("Dia: %s", title);
+    gtk_window_set_title (GTK_WINDOW(dialog), real_title);
+    g_free (real_title);
+  }
+  gtk_widget_show (dialog);
+  g_signal_connect (G_OBJECT (dialog), "response",
+		    G_CALLBACK (gtk_widget_hide),
+		    NULL);
+  msginfo->dialog = dialog;
+  g_signal_connect (G_OBJECT (dialog), "destroy",
+		    G_CALLBACK (message_dialog_destroyed),
+		    msginfo);
+
+  msginfo->repeat_label = gtk_label_new(_("There is one similar message."));
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
+		    msginfo->repeat_label);
+
+  msginfo->show_repeats =
+    gtk_check_button_new_with_label(_("Show repeated messages"));
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
+		    msginfo->show_repeats);
+  g_signal_connect(G_OBJECT(msginfo->show_repeats), "toggled", 
+		   G_CALLBACK(gtk_message_toggle_repeats), msginfo);
+
+  msginfo->repeat_view = gtk_text_view_new();
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
+		    msginfo->repeat_view);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(msginfo->repeat_view), FALSE);
+
+  textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(msginfo->repeat_view));
+  if (msginfo->repeats != NULL) {
+    repeats = msginfo->repeats;
+    repeats = repeats->next;
+    for (; repeats != NULL; repeats = repeats->next) { 
+      gtk_text_buffer_insert_at_cursor(textbuffer, (gchar*)repeats->data, -1);
+    }
+  }
+}
+
+static void
 gtk_message_internal(const char* title, const char *fmt,
                      va_list *args,  va_list *args2)
 {
   static gchar *buf = NULL;
   static gint   alloc = 0;
   gint len;
-  GtkMessageType type = GTK_MESSAGE_INFO;
   DiaMessageInfo *msginfo;
+  GtkTextBuffer *textbuffer;
 
   if (message_hash_table == NULL) {
     message_hash_table = g_hash_table_new(g_str_hash, g_str_equal);
@@ -77,67 +157,31 @@ gtk_message_internal(const char* title, const char *fmt,
   vsprintf (buf, fmt, *args2);
 
   msginfo = (DiaMessageInfo*)g_hash_table_lookup(message_hash_table, fmt);
-  if (msginfo != NULL) {
-    GtkTextBuffer *textbuffer;
-    if (msginfo->repeats == NULL) {
-      GtkWidget *showrepeats;
-      /* First time repeated, create repeat label */
-      msginfo->repeats = g_list_append(msginfo->repeats, buf);
-      msginfo->repeat_label = gtk_label_new(_("There is one similar message."));
-      gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
-			msginfo->repeat_label);
-      gtk_widget_show(msginfo->repeat_label);
-
-      showrepeats = gtk_check_button_new_with_label(_("Show repeated messages"));
-      gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
-			showrepeats);
-      g_signal_connect(G_OBJECT(showrepeats), "toggled", G_CALLBACK(gtk_message_toggle_repeats), msginfo);
-      gtk_widget_show(showrepeats);
-
-      msginfo->repeat_view = gtk_text_view_new();
-      gtk_container_add(GTK_CONTAINER(GTK_DIALOG(msginfo->dialog)->vbox), 
-			msginfo->repeat_view);
-      gtk_text_view_set_editable(GTK_TEXT_VIEW(msginfo->repeat_view), FALSE);
-    } else {
-      char *newlabel;
-      msginfo->repeats = g_list_append(msginfo->repeats, buf);
-      newlabel = g_strdup_printf(_("There are %d similar messages."),
-				       g_list_length(msginfo->repeats));
-      gtk_label_set_text(GTK_LABEL(msginfo->repeat_label), newlabel);
-    }
-    textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(msginfo->repeat_view));
-    gtk_text_buffer_insert_at_cursor(textbuffer, buf, -1);
-    gtk_widget_show (msginfo->dialog);
-  } else {
-    GtkWidget *dialog = NULL;
-    /* quite dirty to not change Dia's message api */
-    if (title) {
-      if (0 == strcmp (title, _("Error")))
-	type = GTK_MESSAGE_ERROR;
-      else if (0 == strcmp (title, _("Warning")))
-	type = GTK_MESSAGE_WARNING;
-    }
-    dialog = gtk_message_dialog_new (NULL, /* no parent window */
-				     0,    /* GtkDialogFlags */
-				     type,
-				     GTK_BUTTONS_CLOSE,
-				     buf);
-    if (title) {
-      gchar *real_title;
-
-      real_title = g_strdup_printf ("Dia: %s", title);
-      gtk_window_set_title (GTK_WINDOW(dialog), real_title);
-      g_free (real_title);
-    }
-    gtk_widget_show (dialog);
-    g_signal_connect (G_OBJECT (dialog), "response",
-		      G_CALLBACK (gtk_widget_hide),
-		      NULL);
-    /* Store relevant info to allow repeats to be collapsed */
+  if (msginfo == NULL) {
     msginfo = g_new0(DiaMessageInfo, 1);
-    msginfo->dialog = dialog;
     g_hash_table_insert(message_hash_table, (char *)fmt, msginfo);
   }
+  if (msginfo->dialog == NULL)
+    message_create_dialog(title, msginfo, buf);
+
+  if (msginfo->repeats != NULL) {
+    if (g_list_length(msginfo->repeats) > 1) {
+      char *newlabel;
+      newlabel = g_strdup_printf(_("There are %d similar messages."),
+				 g_list_length(msginfo->repeats));
+      gtk_label_set_text(GTK_LABEL(msginfo->repeat_label), newlabel);
+    }
+    gtk_widget_show(msginfo->repeat_label);
+    gtk_widget_show(msginfo->show_repeats);
+  }
+
+  /* Insert in scrollable view, but only the repeated ones */
+  if (msginfo->repeats != NULL) {
+    textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(msginfo->repeat_view));
+    gtk_text_buffer_insert_at_cursor(textbuffer, buf, -1);
+  }
+  msginfo->repeats = g_list_append(msginfo->repeats, g_strdup(buf));
+  gtk_widget_show (msginfo->dialog);
 }
 
 static MessageInternal message_internal = gtk_message_internal;
