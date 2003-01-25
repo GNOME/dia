88487119a7726907be17efcea06993dcd28c87c6
Steffen Macke <sdteffen@src.gnome.org>
2003-01-25 07:45:20 +0000

    don't include dlfcn.h on win32

--- plug-ins/xslt/xslt.h
+++ plug-ins/xslt/xslt.h
@@ -21,8 +21,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  */
-
+#include "config.h"
+#ifndef G_OS_WIN32
 #include <dlfcn.h>
+#endif
 #include <glib.h>
 #include <gtk/gtk.h>
 #include "plug-ins.h"
