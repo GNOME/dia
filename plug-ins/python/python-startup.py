3cd15be889f0ba579eaa492ab3015755702e2a61
Hans Breuer <hans@breuer.org>
2007-09-21 10:03:28 +0000

    allow to override system python plug-ins with users version. Patch from
    2007-09-21  Hans Breuer  <hans@breuer.org>
    
    	* plug-ins/python/python-startup.py : allow to override system
    	python plug-ins with users version. Patch from Tom Parker,
    	fixes bug #478554
    
    
    svn path=/trunk/; revision=3786

--- plug-ins/python/python-startup.py
+++ plug-ins/python/python-startup.py
@@ -5,35 +5,30 @@ import dia
 # are quite some PyDia plug-ins which don't require
 # i.e. dont have any (Py)Gtk code
 
-# find system python plugin dir
-curdir = os.path.dirname(__file__)
-plugindir = os.path.join(curdir, 'python')
+loaded = []
 
-#If we're running from source there is no plugin dir
-if not os.path.exists(plugindir):
-    plugindir = curdir
-
-if os.path.isdir(plugindir):
-    # import all plugins found in system plugin dir
-    sys.path.insert(0, plugindir)
-    for file in os.listdir(plugindir):
-	if file[-3:] == '.py':
-	    try:
-		__import__(file[:-3])
-	    except:
-		sys.stderr.write('could not import %s\n' % file)
+def load(plugindir):
+	global loaded
+	if os.path.isdir(plugindir):
+		sys.path.insert(0, plugindir)
+		for file in os.listdir(plugindir):
+			if file[-3:] == '.py' and file[:-3] not in loaded:
+				try:
+					__import__(file[:-3])
+					loaded.append(file[:-3])
+				except:
+					sys.stderr.write('could not import %s\n' % file)
 
 # import any python plugins from the user ...
 if not os.environ.has_key('HOME'):
-    os.environ['HOME'] = os.pathsep + 'tmp'
-plugindir = os.path.join(os.environ['HOME'], '.dia', 'python')
+	os.environ['HOME'] = os.pathsep + 'tmp'
+# import all plugins found in user plugin dir
+load(os.path.join(os.environ['HOME'], '.dia', 'python'))
 
-if os.path.isdir(plugindir):
-    # import all plugins found in user plugin dir
-    sys.path.insert(0, plugindir)
-    for file in os.listdir(plugindir):
-	if file[-3:] == '.py':
-	    try:
-		__import__(file[:-3])
-	    except:
-		sys.stderr.write('could not import %s\n' % file)
+# find system python plugin dir
+curdir = os.path.dirname(__file__)
+plugindir = os.path.join(curdir, 'python')
+#If we're running from source there is no plugin dir
+if not os.path.exists(plugindir):
+	plugindir = curdir
+load(plugindir)
