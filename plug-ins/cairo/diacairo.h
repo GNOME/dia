dcb2e583adf56772a2078003b1e7fdfcac2ce90e
Hans Breuer <hans@breuer.org>
2007-10-07 11:22:08 +0000

    oops, forgot to add this
    2007-10-06  Hans Breuer  <hans@breuer.org>
    
    	* plug-ins/cairo/diacairo.h plug-ins/cairo/Makefile.am : oops, forgot
    	to add this
    
    
    svn path=/trunk/; revision=3803

---
+++ plug-ins/cairo/diacairo.h
@@ -0,0 +1,62 @@
+/* Dia -- an diagram creation/manipulation program
+ * Copyright (C) 1998 Alexander Larsson
+ *
+ * diacairo.c -- Cairo based export plugin for dia
+ * Copyright (C) 2004, 2007 Hans Breuer, <Hans@Breuer.Org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <cairo.h>
+#include "diarenderer.h"
+
+/* --- the renderer base class --- */
+G_BEGIN_DECLS
+
+#define DIA_TYPE_CAIRO_RENDERER           (dia_cairo_renderer_get_type ())
+#define DIA_CAIRO_RENDERER(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), DIA_TYPE_CAIRO_RENDERER, DiaCairoRenderer))
+#define DIA_CAIRO_RENDERER_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST ((klass), DIA_TYPE_CAIRO_RENDERER, DiaCairoRendererClass))
+#define DIA_IS_CAIRO_RENDERER(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), DIA_TYPE_CAIRO_RENDERER))
+#define DIA_CAIRO_RENDERER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), DIA_CAIRO_TYPE_RENDERER, DiaCairoRendererClass))
+
+GType dia_cairo_renderer_get_type (void) G_GNUC_CONST;
+
+typedef struct _DiaCairoRenderer DiaCairoRenderer;
+typedef struct _DiaCairoRendererClass DiaCairoRendererClass;
+
+struct _DiaCairoRenderer
+{
+  DiaRenderer parent_instance;
+
+  cairo_t *cr;
+  cairo_surface_t *surface;
+ 
+  double dash_length;
+  DiagramData *dia;
+
+  real scale;
+  gboolean with_alpha;
+};
+
+struct _DiaCairoRendererClass
+{
+  DiaRendererClass parent_class;
+};
+
+/* FIXME: need to think about proper registration */
+GType dia_cairo_interactive_renderer_get_type (void) G_GNUC_CONST;
+
+G_END_DECLS
+
