a0f35b8ad1183d84352bd7a28096cc07143f1af1
Hans Breuer <hans@breuer.org>
2008-07-26 10:10:05 +0000

    reverted changes from bug #540210, it broke the other use cases, mainly
    2008-07-26  Hans Breuer  <hans@breuer.org>
    
    	* app/display.c(ddisplay_zoom) : reverted changes from bug #540210,
    	it broke the other use cases, mainly zoom all (ctrl^e)
    	
    
    svn path=/trunk/; revision=4098

--- app/display.c
+++ app/display.c
@@ -759,15 +759,11 @@ ddisplay_zoom(DDisplay *ddisp, Point *point, real magnify)
 {
   Rectangle *visible;
   real width, height;
-  real rx, ry;
 
   visible = &ddisp->visible;
 
   width = (visible->right - visible->left)/magnify;
   height = (visible->bottom - visible->top)/magnify;
-  /* calculate cursor position ratios */
-  rx = (point->x - visible->left) / (visible->right - visible->left);
-  ry = (point->y - visible->top) / (visible->bottom - visible->top);
 
   if ((ddisp->zoom_factor <= DDISPLAY_MIN_ZOOM) && (magnify<=1.0))
     return;
@@ -776,8 +772,7 @@ ddisplay_zoom(DDisplay *ddisp, Point *point, real magnify)
 
   ddisp->zoom_factor *= magnify;
 
-  /* set new origin based on the calculated ratios before zooming */
-  ddisplay_set_origo(ddisp, point->x-(width*rx),point->y-(height*ry));
+  ddisplay_set_origo(ddisp, point->x - width/2.0, point->y - height/2.0);
   
   ddisplay_update_scrollbars(ddisp);
   ddisplay_add_update_all(ddisp);
