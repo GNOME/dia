0e449def31359bc0c53df73bd9da774c50691f4e
Hans Breuer <hans@breuer.org>
2009-04-11 14:25:11 +0000

    derive initial scrolling area of properties dialog from screen size, only
    2009-04-11  Hans Breuer  <hans@breuer.org>
    
    	* lib/propdialogs.c : derive initial scrolling area of properties
    	dialog from screen size, only use hardcoded values as fallback
    
    svn path=/trunk/; revision=4377

--- lib/propdialogs.c
+++ lib/propdialogs.c
@@ -283,10 +283,12 @@ prop_dialog_add_properties(PropDialog *dialog, GPtrArray *props)
     GtkRequisition requisition;
     GtkWidget *vbox = prop_dialog_container_pop(dialog);
     GtkWidget *swin = prop_dialog_container_pop(dialog);
-    
+    GdkScreen *screen = gtk_widget_get_screen(swin);
+    guint sheight = screen ? (2 * gdk_screen_get_height(screen)) / 3 : 400;
+
     gtk_widget_size_request (vbox, &requisition);
     /* I'd say default size calculation for scrollable is quite broken */
-    gtk_widget_set_size_request (swin, -1, requisition.height > 400 ? 400 : requisition.height);    
+    gtk_widget_set_size_request (swin, -1, requisition.height > sheight ? sheight : requisition.height);    
   }
 }
 
