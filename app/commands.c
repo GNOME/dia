2e5525e1eb6eb25eb9cda93696d917901847f09e
Steffen Macke <sdteffen@sdteffen.de>
2009-04-17 19:29:29 +0200

    2009-04-17  Steffen Macke <sdteffen@sdteffen.de>
            * app/commands.c: Updated copyright date

--- app/commands.c
+++ app/commands.c
@@ -743,7 +743,7 @@ help_about_callback (GtkAction *action)
         "name", "Dia",
 	"version", VERSION,
 	"comments", _("A program for drawing structured diagrams."),
-	"copyright", "(C) 1998-2008 The Free Software Foundation and the authors",
+	"copyright", "(C) 1998-2009 The Free Software Foundation and the authors",
 	"website", "http://live.gnome.org/Dia",
 	"authors", authors,
 	"documenters", documentors,
