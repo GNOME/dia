737387c7aa87d86567ce9837344f4a5ebedbe898
Hans Breuer <hans@breuer.org>
2009-08-01 21:31:22 +0200

    New test-sizeof (atually just dump sizes)

---
+++ tests/test-sizeof.c
@@ -0,0 +1,27 @@
+/* not really a test but only an interactive check */
+
+#include "config.h"
+#include <stdio.h>
+
+#include "object.h"
+#include "connection.h"
+#include "element.h"
+#include "../objects/UML/class.h"
+
+int
+main (int argc, char** argv)
+{
+#define DUMP(o) g_print ("%s: %d\n", #o, sizeof(##o))
+
+  DUMP(DiaObject);
+  DUMP(Connection);
+  DUMP(Element);
+  DUMP(UMLClass);
+
+  DUMP(Handle);
+  DUMP(ConnectionPoint);
+
+#undef DUMP
+
+  return 0;
+}
