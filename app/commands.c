98273fce952638bc1ce84517ef40e98251aa53ea
Alexandre Prokoudine <alexandre.prokoudine@gmail.com>
2011-06-04 17:06:35 +0400

    Updated copyright years. It's (c) 1998-2011, actually.

--- app/commands.c
+++ app/commands.c
@@ -953,7 +953,7 @@ help_about_callback (GtkAction *action)
         "name", "Dia",
 	"version", VERSION,
 	"comments", _("A program for drawing structured diagrams."),
-	"copyright", "(C) 1998-2009 The Free Software Foundation and the authors",
+	"copyright", "(C) 1998-2011 The Free Software Foundation and the authors",
 	"website", "http://live.gnome.org/Dia",
 	"authors", authors,
 	"documenters", documentors,
