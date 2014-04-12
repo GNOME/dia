a5d09c7f527b6df7e72f52cf6a443e3bd7b25dcc
Hans Breuer <hans@breuer.org>
2014-04-12 22:11:25 +0200

    Fix line endings

--- lib/prop_sdarray_widget.h
+++ lib/prop_sdarray_widget.h
@@ -1,1 +1,4 @@
-/* internal */WIDGET *_arrayprop_get_widget(ArrayProperty *prop, PropDialog *dialog);void    _arrayprop_reset_widget(ArrayProperty *prop, WIDGET *widget);void    _arrayprop_set_from_widget(ArrayProperty *prop, WIDGET *widget);
+/* internal */
+WIDGET *_arrayprop_get_widget(ArrayProperty *prop, PropDialog *dialog);
+void    _arrayprop_reset_widget(ArrayProperty *prop, WIDGET *widget);
+void    _arrayprop_set_from_widget(ArrayProperty *prop, WIDGET *widget);
