ef5f58bfba10053a635daa0e78e20660ccd37374
Edênis Freindorfer Azevedo <edenisfa@gmail.com>
2018-09-02 19:28:31 -0300

    Fix indentation for plug-ins/python/python-startup.py

--- plug-ins/python/python-startup.py
+++ plug-ins/python/python-startup.py
@@ -8,35 +8,35 @@ import dia
 loaded = []
 
 def load(plugindir):
-    global loaded
-    if os.path.isdir(plugindir):
-        sys.path.insert(0, plugindir)
-        for file in os.listdir(plugindir):
-            if file[-3:] == '.py' and file[:-3] not in loaded:
-                try:
-                    __import__(file[:-3])
-                    loaded.append(file[:-3])
-                except Exception as e:
-                    sys.stderr.write('could not import %s [%s]\n' % (file, e))
+  global loaded
+  if os.path.isdir(plugindir):
+    sys.path.insert(0, plugindir)
+    for file in os.listdir(plugindir):
+      if file[-3:] == '.py' and file[:-3] not in loaded:
+        try:
+          __import__(file[:-3])
+          loaded.append(file[:-3])
+        except Exception as e:
+          sys.stderr.write('could not import %s [%s]\n' % (file, e))
 
 # import any python plugins from the user ...
 if not os.environ.has_key('HOME'):
-    os.environ['HOME'] = os.pathsep + 'tmp'
-    os.environ['XDG_DATA_HOME'] = os.pathsep + 'tmp'
+  os.environ['HOME'] = os.pathsep + 'tmp'
+  os.environ['XDG_DATA_HOME'] = os.pathsep + 'tmp'
 else:
-    default = os.path.join(os.environ['HOME'], '.local', 'share')
-    oldlocation = os.path.join(os.environ['HOME'], '.dia')
-    newlocation = os.path.join (
-        os.getenv ('XDG_DATA_HOME', default),
-        'dia'
-    )
-    if os.path.isdir(oldlocation):
-        try:
-            os.rename(oldlocation, newlocation)
-        except OSError:
-            sys.stderr.write('Could not migrate python scripts from \
-                "%s" to "%s". Check if %s is empty.' %
-            (oldlocation, newlocation, newlocation))
+  default = os.path.join(os.environ['HOME'], '.local', 'share')
+  oldlocation = os.path.join(os.environ['HOME'], '.dia')
+  newlocation = os.path.join (
+    os.getenv ('XDG_DATA_HOME', default),
+    'dia'
+  )
+  if os.path.isdir(oldlocation):
+    try:
+      os.rename(oldlocation, newlocation)
+    except OSError:
+      sys.stderr.write('Could not migrate python scripts from \
+                       "%s" to "%s". Check if %s is empty.' %
+                       (oldlocation, newlocation, newlocation))
 
 # import all plugins found in user plugin dir
 load(os.path.join(os.environ['XDG_DATA_HOME'], 'dia', 'python'))
@@ -46,5 +46,5 @@ curdir = os.path.dirname(__file__)
 plugindir = os.path.join(curdir, 'python')
 #If we're running from source there is no plugin dir
 if not os.path.exists(plugindir):
-    plugindir = curdir
+  plugindir = curdir
 load(plugindir)
