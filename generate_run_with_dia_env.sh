4333ff182aa0a69ee8840cd29a6d4d599235d5f3
Eduard Nicodei <eddnicodei@gmail.com>
2019-01-06 21:05:40 +0000

    #19: Windows: Use ';' for separating paths in run_with_dia.

--- generate_run_with_dia_env.sh
+++ generate_run_with_dia_env.sh
@@ -6,18 +6,23 @@ fi
 SOURCE_ROOT=$2
 BUILD_ROOT=$3
 
+# Windows has ; for PATH separators.
+PATH_SEP=":"
+if uname | grep -iq MINGW; then
+    PATH_SEP=";"
+fi
 # This unfortunately is a duplication of run_env in
 # tests/meson.build.  This is fine as long as this
 # script is only used to generate run_with_dia_env
 # and only that.
 cat > $1 << EOF
 #!/usr/bin/env sh
-export DIA_BASE_PATH=${SOURCE_ROOT}
-export DIA_LIB_PATH=${BUILD_ROOT}/objects:${BUILD_ROOT}/plug-ins
-export DIA_SHAPE_PATH=${SOURCE_ROOT}/shapes
-export DIA_XSLT_PATH=${SOURCE_ROOT}/plug-ins/xslt
-export DIA_PYTHON_PATH=${SOURCE_ROOT}/plug-ins/python
-export DIA_SHEET_PATH=${BUILD_ROOT}/sheets
+export DIA_BASE_PATH="${SOURCE_ROOT}"
+export DIA_LIB_PATH="${BUILD_ROOT}/objects${PATH_SEP}${BUILD_ROOT}/plug-ins"
+export DIA_SHAPE_PATH="${SOURCE_ROOT}/shapes"
+export DIA_XSLT_PATH="${SOURCE_ROOT}/plug-ins/xslt"
+export DIA_PYTHON_PATH="${SOURCE_ROOT}/plug-ins/python"
+export DIA_SHEET_PATH="${BUILD_ROOT}/sheets"
 \$@
 EOF
 
