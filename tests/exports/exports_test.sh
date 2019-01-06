1113f97078d3979e2d8f4a474be74f1584ce6037
Eduard Nicodei <eddnicodei@gmail.com>
2019-01-06 22:51:44 +0000

    #19: Code formatting spaces vs tabs: exports_test.sh.

--- tests/exports/exports_test.sh
+++ tests/exports/exports_test.sh
@@ -12,19 +12,20 @@ fi
 FAILED=0
 TEMP_DIR=$(mktemp -d)
 for FILTER in ${EXPORT_PLUGINS}; do
-	for FILE in *.dia; do
+    for FILE in *.dia; do
         DIA_BASENAME=$(basename "${FILE}" .dia)
-		OUTPUT=${TEMP_DIR}/${DIA_BASENAME}.${FILTER}
-		EXPECTED_OUTPUT=${FILTER}/${DIA_BASENAME}.${FILTER}
-		if ! ${RUN_DIA} -t ${FILTER} -e ${OUTPUT} ${FILE}; then
-			echo ${FILTER} failed for ${FILE};
+        OUTPUT=${TEMP_DIR}/${DIA_BASENAME}.${FILTER}
+        EXPECTED_OUTPUT=${FILTER}/${DIA_BASENAME}.${FILTER}
+        if ! ${RUN_DIA} -t ${FILTER} -e ${OUTPUT} ${FILE}; then
+            echo ${FILTER} failed for ${FILE};
+            rm -r ${TEMP_DIR}
             exit 1
-		elif ! diff -q ${OUTPUT} ${EXPECTED_OUTPUT}; then
-			echo "FAILED: ${FILTER} output different from expected for ${FILE}" 
+        elif ! diff -q ${OUTPUT} ${EXPECTED_OUTPUT}; then
+            echo "FAILED: ${FILTER} output different from expected for ${FILE}"
             #TODO: currently this fails almost all testcases.  Why?
-            FAILED=1
-		fi;
-	done;
+            FAILED=3
+        fi;
+    done;
 done
 
 rm -r ${TEMP_DIR}
