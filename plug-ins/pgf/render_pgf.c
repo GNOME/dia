6e2902a44760843fda00643f17da5a1c3194f8b5
Josef Kufner <josef@kufner.cz>
2019-01-30 21:49:10 +0100

    Fix TeX escaping for very short strings

--- plug-ins/pgf/render_pgf.c
+++ plug-ins/pgf/render_pgf.c
@@ -1092,14 +1092,14 @@ tex_escape_string(const gchar *src, DiaContext *ctx)
     int len = g_utf8_strlen(src, -1);
     GString *dest = g_string_sized_new(len);
     gchar *p;
-    char b = src[0], e = src[len - 1];
+    char b = src[0], e = len > 2 ? src[len - 1] : 0;
 
     if (!g_utf8_validate(src, -1, NULL)) {
 	dia_context_add_message(ctx, _("Not valid UTF-8"));
 	return g_strdup(src);
     }
 
-    // Do not escape TeX macros if string is marked properly -- "$...$" or "{...}"
+    /* Do not escape TeX macros if string is marked properly -- "$...$" or "{...}" */
     if ((b == '$' && e == '$') || (b == '{' && e == '}')) {
         return g_strdup(src);
     }
