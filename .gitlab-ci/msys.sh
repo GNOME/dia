48ca1d0a6c111967d654208cd5e3379f54646163
Hubert Figuière <hub@figuiere.net>
2021-09-08 22:39:19 -0400

    msys: fix gitlab-ci

--- .gitlab-ci/msys.sh
+++ .gitlab-ci/msys.sh
@@ -29,7 +29,7 @@ pacman --noconfirm -S --needed \
     mingw-w64-$MSYS2_ARCH-graphene \
     mingw-w64-$MSYS2_ARCH-pango \
     mingw-w64-$MSYS2_ARCH-shared-mime-info \
-    mingw-w64-$MSYS2_ARCH-gtk2
+    mingw-w64-$MSYS2_ARCH-gtk3
 
 mkdir -p _ccache
 export CCACHE_BASEDIR="$(pwd)"
